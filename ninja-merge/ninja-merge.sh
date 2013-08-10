be909b4d5b706db8a2d51ffa206c8ff61b3209ad
Dan Fruehauf <malkodan@gmail.com>
2013-08-10 21:05:56 +1000

    using shunit2 for unit testing

--- ninja-merge/ninja-merge.sh
+++ ninja-merge/ninja-merge.sh
@@ -40,142 +40,6 @@ declare -r CHECKSUM_TYPE=md5sum
 # global variable to dictate whether we should have debug messages on
 declare DEBUG=no
 
-# helper function to display a test message
-# $1 - retval
-# $2 - fatal? yes/no
-# "$@" - test name
-test_message() {
-	local -i retval=$1; shift
-	local fatal=$1; shift
-	echo -n "$@..."
-	if [ $retval -eq 0 ]; then
-		echo "OK"
-	else
-		echo "FAILED"
-		# quit if it's a fatal error
-		[ "$fatal" = "yes" ] && exit $retval
-	fi
-}
-
-# perform a little unit test
-# $1 - source directory
-# $2 - destination directory
-run_tests() {
-	local src_dir=$1; shift
-	local index_dir=/tmp/test-index; mkdir -p $index_dir
-
-	# copy directories to a temporary place
-	# src dir
-	local src_dir_tmp=`mktemp -d -u`
-	cp -a "$src_dir" $src_dir_tmp
-	echo "Running tests with source directory: '$src_dir_tmp'" 1>&2
-
-	# dest dir
-	local dst_dir_tmp=`mktemp -d -u`
-	cp -a "$src_dir" $dst_dir_tmp
-	echo "Running tests with destination directory: '$dst_dir_tmp'" 1>&2
-
-	# inspection dir
-	local inspect_dir_tmp=`mktemp -d`
-	echo "Running tests with inspection directory: '$inspect_dir_tmp'" 1>&2
-
-	# inject a file in the source directory, a file which will not exist on
-	# the destination directory and will be needed to be COPIED
-	echo "I HAVE TO BE COPIED" > "$src_dir_tmp/COPY ME PLEASE"
-	
-	# inject a file in the destination directory, and one which differs in
-	# contents in the source directory - a file which will be neede to
-	# be INSPECTED
-	echo "I HAVE TO BE INSPECTED" > "$dst_dir_tmp/INSPECT ME PLEASE"
-	echo "I HAVE TO BE INSPECTED BECAUSE I HAVE DIFFERENT CONTENTS" > \
-		"$src_dir_tmp/INSPECT ME PLEASE"
-
-	# remove old indexes if they exist in test directories
-	rm -f $dst_db $src_db
-
-	# tests pretty much begin here
-	local -i retval=0
-	echo
-	echo "Running Tests:"
-	echo "--------------"
-
-	##############################
-	# TEST #1: merge directories #
-	##############################
-	merge_directories $src_dir_tmp $dst_dir_tmp $inspect_dir_tmp
-	local retval=$?; test_message $retval yes "Merge directories"
-
-	#########################################
-	# TEST #2: verify files to be inspected #
-	#########################################
-	# alright, verify results
-	retval=0
-	local inspect_file_checksum=`md5sum "$src_dir_tmp/INSPECT ME PLEASE" | cut -d' ' -f1`
-	! test -f "$inspect_dir_tmp/INSPECT ME PLEASE.$inspect_file_checksum" && retval=1
-	test_message $retval no "Files to be inspected"
-
-	#############################
-	# TEST #3: second collision #
-	#############################
-	# test collision on collision
-	# if you see a warning here while running, it's all good!
-	echo "I HAVE TO BE INSPECTED BECAUSE I HAVE DIFFERENT CONTENTS OK???" > \
-		"$src_dir_tmp/INSPECT ME PLEASE"
-	merge_directories $src_dir_tmp $dst_dir_tmp $inspect_dir_tmp
-	local inspect_file_checksum=`md5sum "$src_dir_tmp/INSPECT ME PLEASE" | cut -d' ' -f1`
-	! test -f "$inspect_dir_tmp/INSPECT ME PLEASE.$inspect_file_checksum" && retval=1
-	test_message $retval no "Files to be inspected ^ 2"
-	rm -f "$src_dir_tmp/INSPECT ME PLEASE"
-
-	#####################################################
-	# TEST #4: same size, same name, different contents #
-	#####################################################
-	# test collision, same size
-	retval=0
-	echo "1234" > \
-		"$src_dir_tmp/INSPECT SAME SIZE"
-	echo "4567" > \
-		"$dst_dir_tmp/INSPECT SAME SIZE"
-	local src_inspect_file_checksum=`md5sum "$src_dir_tmp/INSPECT SAME SIZE" | cut -d' ' -f1`
-	local dst_inspect_file_checksum=`md5sum "$dst_dir_tmp/INSPECT SAME SIZE" | cut -d' ' -f1`
-	merge_directories $src_dir_tmp $dst_dir_tmp $inspect_dir_tmp
-	! test -f "$inspect_dir_tmp/INSPECT SAME SIZE.$src_inspect_file_checksum" && retval=1
-	test_message $retval no "Files to be inspected ^ 3"
-	rm -f "$src_dir_tmp/INSPECT SAME SIZE"
-
-
-	######################################
-	# TEST #5: verify files to be copied #
-	######################################
-	# files to be copied
-	retval=0
-	! test -f "$dst_dir_tmp/COPY ME PLEASE" && retval=1
-	test_message $retval no "Files to be copied"
-
-	#############################
-	# TEST #6: injecting a file #
-	#############################
-	retval=0
-	echo "I HAVE BEEN INJECTED AND I NEED TO BE COPIED" > "$src_dir_tmp/COPY ME PLEASE AFTER UPDATE"
-	merge_directories $src_dir_tmp $dst_dir_tmp $inspect_dir_tmp
-	! test -f "$dst_dir_tmp/COPY ME PLEASE AFTER UPDATE" && retval=1
-	test_message $retval yes "Update directory"
-
-	################################
-	# TEST #7: override empty file #
-	################################
-	retval=0
-	echo "I HAVE CONTENTS" > "$src_dir_tmp/EMPTY FILE COLLISION"
-	touch "$dst_dir_tmp/EMPTY FILE COLLISION"
-	merge_directories $src_dir_tmp $dst_dir_tmp $inspect_dir_tmp
-	[ `wc -c "$dst_dir_tmp/EMPTY FILE COLLISION" | cut -d' ' -f1` -eq 0 ] && reval=1
-	test_message $retval yes "Empty file collision"
-
-	# cleanup!
-	rm -rf --preserve-root "$src_dir_tmp" "$dst_dir_tmp" "$inspect_dir_tmp"
-	exit $retval
-}
-
 # copy file to inspection directory
 # $1 - src_dir
 # $2 - dst_dir (usually inspection directory)
@@ -324,8 +188,6 @@ Options:
   -i, --inspection           Inspection directory - all rejects will end up
                              here.
   -o, --sort                 Sort source directories by size before copying.
-  -t, --test                 Run test suite. Still needs source and destination
-                             directories, but doesn't modify them.
   -m, --mail                 Send email to this address after execution.
   -v, --verbose              Print more debug messages."
 	exit 2
@@ -335,12 +197,11 @@ Options:
 # arguments will be parsed with getopt, see usage()
 main() {
 	# parse options with getopt
-	local tmp_getops=`getopt -o hs:d:i:otm:v --long help,source:,destination:,sort,test,mail:,verbose -- "$@"`
+	local tmp_getops=`getopt -o hs:d:i:om:v --long help,source:,destination:,sort,mail:,verbose -- "$@"`
 	[ $? != 0 ] && usage
 
 	eval set -- "$tmp_getops"
 	local src_dirs dst_dir inspect_dir mail
-	local test=no
 	local sort=no
 
 	# parse the options
@@ -351,7 +212,6 @@ main() {
 			-d|--destination) dst_dir="$2"; shift 2;;
 			-i|--inspection) inspect_dir="$2"; shift 2;;
 			-o|--sort) sort="yes"; shift 1;;
-			-t|--test) test="yes"; shift 1;;
 			-m|--mail) mail="$2"; shift 2;;
 			-v|--verbose) DEBUG="yes"; shift 1;;
 			--) shift; break;;
@@ -362,10 +222,6 @@ main() {
 	# make sure src_dir exists
 	src_dirs=`echo $src_dirs | sed -e 's/^ //g'`
 	[ x"$src_dirs" = x ] && usage
-
-	# test only? needs only src_dir
-	# exits after test
-	[ "$test" = "yes" ] && run_tests "$src_dirs"
 
 	# make sure dst_dir exists
 	[ x"$dst_dir" = x ] && usage
---
+++ ninja-merge/shunit2_test.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+#
+# ninja-merge.sh - a rsync wrapper which handles duplicate files better
+# Copyright (C) 2013 Dan Fruehauf <malkoadan@gmail.com>
+# Copyright (C) 2013 IMOS <imos.org.au>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+# test a merge from source directory to a destination directory
+test_merge_to_empty_directory() {
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+
+	local -i diff_lines=`diff -urN $SOURCE_DIR $DEST_DIR | wc -l`
+	assertTrue 'destination not identical to source' "[ $diff_lines -eq 0 ]"
+
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+	local -i diff_lines=`diff -urN $SOURCE_DIR $DEST_DIR | wc -l`
+	assertTrue 'destination not identical to source after running merge again' \
+		"[ $diff_lines -eq 0 ]"
+
+	local -i inspect_file_nr=`ls -1 $INSPECT_DIR | wc -l`
+	assertTrue 'inspection directory has no files' \
+		"[ $inspect_file_nr -eq 0 ]"
+}
+
+# test a merge to an identical directory
+test_merge_to_identical_directory() {
+	cp -a $SOURCE_DIR/* $DEST_DIR/
+
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+
+	local -i diff_lines=`diff -urN $SOURCE_DIR $DEST_DIR | wc -l`
+	assertTrue 'destination not identical to source' "[ $diff_lines -eq 0 ]"
+}
+
+# make sure we can run ninja merge over and over and its idempotent
+test_idempotency() {
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+
+	local -i diff_lines=`diff -urN $SOURCE_DIR $DEST_DIR | wc -l`
+	assertTrue 'destination not identical to source' "[ $diff_lines -eq 0 ]"
+
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+	local -i diff_lines=`diff -urN $SOURCE_DIR $DEST_DIR | wc -l`
+	assertTrue 'destination not identical to source after running merge again' \
+		"[ $diff_lines -eq 0 ]"
+
+}
+
+# test a merge to a directory with a canary file
+test_canary_file_copied_in_merge() {
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+
+	rm -f "$DEST_DIR/COPY ME PLEASE"
+	echo "COPY ME!!" > "$SOURCE_DIR/COPY ME PLEASE"
+
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+
+	assertTrue 'canary file copied in merge' \
+		"[ -f \"$DEST_DIR/COPY ME PLEASE\" ]"
+}
+
+# inject a file to be inspected
+test_files_to_be_inspected() {
+	# inject file in destination directory
+	echo "I HAVE TO BE INSPECTED" > "$DEST_DIR/INSPECT ME PLEASE"
+
+	# inject file with same name in source directory
+	echo "I HAVE TO BE INSPECTED BECAUSE I HAVE DIFFERENT CONTENTS" > \
+		"$SOURCE_DIR/INSPECT ME PLEASE"
+	local inspect_file_checksum=`md5sum "$SOURCE_DIR/INSPECT ME PLEASE" | cut -d' ' -f1`
+
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+
+	assertTrue 'source file to be inspected exists in inspection directory' \
+		"[ -f \"$INSPECT_DIR/INSPECT ME PLEASE.$inspect_file_checksum\" ]"
+
+	local inspect_file_checksum=`md5sum "$DEST_DIR/INSPECT ME PLEASE" | cut -d' ' -f1`
+	assertTrue 'destination file to be inspected exists in inspection directory' \
+		"[ -f \"$INSPECT_DIR/INSPECT ME PLEASE.$inspect_file_checksum\" ]"
+
+
+	# test collision on collision
+	# 'INSPECT ME PLEASE' will already exist in the inspection directory
+	# make sure we handle another collision with the same name
+	# if you see a warning here while running, it's all good!
+	echo "I HAVE TO BE INSPECTED BECAUSE I HAVE DIFFERENT CONTENTS OK???" > \
+		"$SOURCE_DIR/INSPECT ME PLEASE"
+
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+
+	local inspect_file_checksum=`md5sum "$SOURCE_DIR/INSPECT ME PLEASE" | cut -d' ' -f1`
+	assertTrue 'file to be inspected exists in inspection directory' \
+		"[ -f \"$INSPECT_DIR/INSPECT ME PLEASE.$inspect_file_checksum\" ]"
+}
+
+# inject a file to be inspected with same size
+test_files_to_be_inspected_same_size() {
+	# test collision, same size
+	echo "1234" > \
+		"$SOURCE_DIR/INSPECT SAME SIZE"
+	echo "4567" > \
+		"$DEST_DIR/INSPECT SAME SIZE"
+
+	local src_inspect_file_checksum=`md5sum "$SOURCE_DIR/INSPECT SAME SIZE" | cut -d' ' -f1`
+	local dst_inspect_file_checksum=`md5sum "$DEST_DIR/INSPECT SAME SIZE" | cut -d' ' -f1`
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+
+	assertTrue 'source file to be inspected exists in inspection directory' \
+		"[ -f \"$INSPECT_DIR/INSPECT SAME SIZE.$src_inspect_file_checksum\" ]"
+	assertTrue 'destination file to be inspected exists in inspection directory' \
+		"[ -f \"$INSPECT_DIR/INSPECT SAME SIZE.$dst_inspect_file_checksum\" ]"
+}
+
+# make sure we override empty files and prefer files with content
+test_override_empty_file_in_destination() {
+	echo "I HAVE CONTENTS" > "$SOURCE_DIR/EMPTY FILE COLLISION"
+	touch "$DEST_DIR/EMPTY FILE COLLISION"
+	local src_file_checksum=`md5sum "$SOURCE_DIR/EMPTY FILE COLLISION" | cut -d' ' -f1`
+
+	$NINJA_MERGE_EXEC -s $SOURCE_DIR -d $DEST_DIR -i $INSPECT_DIR >& /dev/null
+
+	# make sure no files are in the inspection directory
+	local -i files_in_inspection_directory=`ls -1 $INSPECT_DIR | wc -l`
+	assertTrue 'inspection directory empty' \
+		"[ $files_in_inspection_directory -eq 0 ]"
+
+	# test if destination file was overriden
+	local dst_file_checksum=`md5sum "$DEST_DIR/EMPTY FILE COLLISION" | cut -d' ' -f1`
+	assertTrue 'destination file overriden' \
+		"[ "$src_file_checksum" = "$dst_file_checksum" ]"
+}
+
+
+##################
+# SETUP/TEARDOWN #
+##################
+
+oneTimeSetUp() {
+	# load include to test
+	NINJA_MERGE_EXEC=`dirname $0`/ninja-merge.sh
+	SOURCE_SETUP_DIR=`mktemp -d`
+	# should be a directory with some files...
+	REAL_SOURCE_DIRECTORY=/etc/sysconfig
+	(cp -a $REAL_SOURCE_DIRECTORY/* $SOURCE_SETUP_DIR >& /dev/null)
+	DEST_DIR=`mktemp -d`
+}
+
+oneTimeTearDown() {
+	rm -rf --preserve-root $SOURCE_SETUP_DIR $DEST_DIR
+}
+
+setUp() {
+	SOURCE_DIR=`mktemp -d`
+	INSPECT_DIR=`mktemp -d`
+	cp -a $SOURCE_SETUP_DIR/* $SOURCE_DIR/
+}
+
+tearDown() {
+	rm -rf --preserve-root $SOURCE_DIR $INSPECT_DIR
+}
+
+# load and run shUnit2
+. /usr/share/shunit2/shunit2
